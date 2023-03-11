384c2f7ef6ba8b54bb662acce4165ead17e06bdd
santiagosayshey <chaucted@gmail.com>
2023-03-12 02:37:31 +1030

    making progress on adding code snippet

--- custom-format.js
+++ custom-format.js
@@ -1,9 +1,26 @@
 function codeBlockName(name) {
   var header = document.getElementById("cf-block-name")
-  header.innerText = name;
-  
+  header.innerText = name+":";
+  populateCodeBlock(name);
 }
 
 function populateCodeBlock(name) {
-
+  var block = document.getElementById("block-json").innerHTML = JSON.stringify(MA,null,' ');
 }
+
+var MA = {
+  "name": "Movies Anywhere",
+  "includeCustomFormatWhenRenaming": true,
+  "specifications": [
+    {
+      "name": "Movies Anywhere",
+      "implementation": "ReleaseTitleSpecification",
+      "negate": false,
+      "required": true,
+      "fields": {
+        "value": 
+	        "(?&lt!dts[ .-]?hd[ .-]?)ma\\b(?=.*\\bweb[ ._-]?(dl|rip)\\b)"
+      }
+    }
+  ]
+};
